8304ab16fd79245532ca6c83cc453e71cfc8754b
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-01 18:43:11 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('Connected to MongoDB!!!');
+}).catch((err) => {
+    console.log(err);
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
