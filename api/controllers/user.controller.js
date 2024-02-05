db2a3a1f493caba7a9f945a8ff5821e12edc5a42
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-05 17:01:34 +0530

    Corrected error User model did not import  in user.controller.js

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
 
 export const test = (req, res) => {
     res.json({
