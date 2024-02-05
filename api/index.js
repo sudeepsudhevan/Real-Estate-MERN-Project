34e85d0f6ce4371fa4e501f5394dada6997709aa
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-05 16:52:09 +0530

    Create user update api

--- api/index.js
+++ api/index.js
@@ -3,12 +3,15 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log('Connected to MongoDB!!!');
@@ -20,6 +23,8 @@ app.use("/api/user", userRouter);
 
 app.use("/api/auth", authRouter);
 
+
+
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
@@ -29,6 +34,8 @@ app.use((err, req, res, next) => {
         message
     });
 })
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
