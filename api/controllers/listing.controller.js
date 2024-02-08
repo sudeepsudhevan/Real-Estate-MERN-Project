42121b1ea9d4128df4fa5f8f3512243eaa94a1e0
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-08 17:59:26 +0530

    Create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,6 +1,7 @@
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
+
 export const createListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body);
@@ -28,3 +29,22 @@ export const deleteListing = async (req, res, next) => {
         next(error);
     }
 };
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, "Listing not found!"));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, "You are not authorized to update this listing!"));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+}
